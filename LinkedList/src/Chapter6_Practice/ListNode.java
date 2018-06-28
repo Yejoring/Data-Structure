578fd93faccada1734b1433d099b37194cebbed8
Yejoring <why.tt.shit@gmail.com>
2018-06-28 14:54:13 +0900

    chapter 6 연습문제 4번 푸는중...

---
+++ LinkedList/src/Chapter6_Practice/ListNode.java
@@ -0,0 +1,5 @@
+package Chapter6_Practice;
+
+public class ListNode {
+
+}
---
+++ LinkedList/src/Chapter6_Practice/ch6_4.java
@@ -0,0 +1,13 @@
+package Chapter6_Practice;
+
+import java.util.ArrayList;
+
+public class ch6_4 {
+	public static void main(String[] args) {
+		LinkedList l1=new LinkedList();
+		ArrayList<PeopleInfo> ap1=new ArrayList<PeopleInfo>();
+		
+		//±ÍÂú¾Æ....
+		
+	}
+}
