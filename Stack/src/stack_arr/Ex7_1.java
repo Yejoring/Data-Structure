1fd44fc35a5840328672980351fc1aaf212011bc
Yejoring <why.tt.shit@gmail.com>
2018-07-11 11:10:39 +0900

    Array Stack main : 주석 추가

--- Stack/src/stack_arr/Ex7_1.java
+++ Stack/src/stack_arr/Ex7_1.java
@@ -11,20 +11,20 @@ public class Ex7_1 {
 		final int PRINT=5;
 		final int EXIT=0;
 		Scanner sc=new Scanner(System.in);
-		int num;
-		char data;
-		boolean bIterateFlag=true;
-		StackMenu menu=new StackMenu();
+		int num;			// 메뉴 번호 입력
+		char data;			// 삽입할 데이터 입력
+		boolean bIterateFlag=true;		// 반복문의 Flag
+		StackMenu menu=new StackMenu();	// 메뉴 번호 입력 받기 위한 Scanner객체 생성
 		System.out.print("스택 사이즈 입력 : ");
-		num=sc.nextInt();
+		num=sc.nextInt();		// 지정한 스택사이즈만큼 생성
 		ArrayStack stack=new ArrayStack(num);
 		while(bIterateFlag) {
-			menu.printStackMenu();
-			num=sc.nextInt();
+			menu.printStackMenu();		// 메뉴 출력
+			num=sc.nextInt();			
 			switch(num) {
 			case PUSH:
 				menu.printPushMenu();
-				data=sc.next().toCharArray()[0];
+				data=sc.next().toCharArray()[0];	// Scanner 객체로는 char형 입력 못받아서 일단 CharArray로 바꾼 뒤 0번째 인덱스 추출
 				stack.push(data);
 				break;
 			case POP:
